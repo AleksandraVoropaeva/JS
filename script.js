48ceb49b2ecff006a38ef07d9ea2a279c2f0cad6
Aleksandra <a.voropaeva88@gmail.com>
2018-11-16 01:59:05 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let days = 30;
+let expensesMandatory = prompt("Введите обязательную статью расходов в этот месяце");
+let expensesHowMatch = prompt("Во сколько обойдется?");
+let expensesMandatory2 = prompt("Введите еще одну обязательную статью расходов в этот месяце");
+let expensesHowMatch2 = prompt("Во сколько обойдется?");
+
+let appData = {
+    money: money,
+    timeData: time,
+    expenses: {
+        // function(){
+        //     console.log("expensesMandatory");
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+console.log(appData["money"]);
+console.log(appData["timeData"]);
+console.log(appData["expenses"]);
+
+
+alert( +money / days );
---
+++ script2.js
@@ -0,0 +1,173 @@
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// // console.log(n1);
+
+// var und;
+// console.log(und);
+
+// obj = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false
+// }
+
+// console.log(obj["name"])
+
+// var arr = ['plum', 'orange', 'apple'];
+
+// console.log(arr[2]);
+
+// // alert("Ты сможешь.");
+
+// confirm("Are you here?");
+
+// var answer = +prompt("Вам есть 18?", "18");
+
+// console.log( typeof(answer));
+
+// console.log('arr' + ' - object');
+// console.log('4' + ' - object');
+
+// var incr = 10;
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(incr++);
+// // console.log(decr--);    
+
+// console.log(5%2);
+
+// console.log("10" === "10");
+
+// var isChecked = true;
+// var isClose = true;
+
+// console.log(isChecked || !isClose); 
+
+// // таблица приотритетов!
+
+
+// console.log(y);
+// var y = 5;
+
+
+// // const pi = 3.14;
+// // pi = 2;
+// console.log(5*2);
+
+
+// let n = 10;
+// n--;
+// // console.log(n);
+// // console.log(11%3);
+
+// // let sum = 5 * 5 + 10 % 3;
+// // console.log(sum); 
+
+// let a = 5;
+// console.log(a !== 4);
+
+// let number = 5;
+
+// console.log(5/0);
+// // console.log(5 / "string");
+
+// let x = 5;
+// console.log(x == 2);
+// console.log(x == 5);
+
+// let nothing;
+// // console.log(something);
+
+// let user = {
+// 	name : "Aleksandra",
+// 	age : 24,
+// 	human : true,
+// 	"favorite book" : "Extremely loud and incredible close",
+// 	breathe : function(){
+// 		console.log("I am alive!")
+// 	}
+// }
+
+// console.log(user);
+
+
+// console.log(user.name);
+// console.log(user['favorite book']);
+
+
+
+// let arr = ["plum", "watermelon", "apple"];
+
+// console.log(arr[0]);
+// // console.log(arr[1]);
+// // console.log(arr[2]);
+
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof 0);
+// console.log(typeof false);
+// console.log(typeof "string");
+// console.log(typeof underfined);
+// // console.log(typeof Symbol());
+// // console.log(typeof null);
+
+// let counter = 10;
+
+// // if (counter == 4) {
+// // 	console.log(counter);
+// // }
+// // else {
+// // 	console.log(0)
+// // }
+
+
+// // if (counter == 0) {
+// // 	console.log("Nothing to do here");
+// // } else if (counter < 0) {
+// // 	console.log("Error");
+// // } else {
+// // 	console.log("In progress!")
+// // }
+
+
+// let message = (counter < 0) ? ("Error!") : (counter == 0) ? ("Not bad") : ("In progress!");
+
+// console.log(message);
+
+// switch(counter) {
+// 	case 0:
+// 	console.log("Nothing to do here");
+// 	break;
+
+// 	case -1:
+// 	console.log("Error");
+// 	break;
+
+// 	case 5:
+// 	console.log("in progress!");
+// 	break;
+
+// 	default:
+// 	console.log("??? love you")
+// 	break;
+
+// }
+
+let counter = 5;
+
+while (counter < 8) {
+	console.log(counter);
+	counter++;
+}
+
