156d39b8cab67b215b4cedea8c740d309b472764
Aleksandra <a.voropaeva88@gmail.com>
2018-11-16 21:09:58 +0300

    Правки первый урок

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,27 +1,25 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 let days = 30;
-let expensesMandatory = prompt("Введите обязательную статью расходов в этот месяце");
+let expensesMustBe = prompt("Введите обязательную статью расходов в этот месяце");
 let expensesHowMatch = prompt("Во сколько обойдется?");
-let expensesMandatory2 = prompt("Введите еще одну обязательную статью расходов в этот месяце");
+let expensesMustBe2 = prompt("Введите еще одну обязательную статью расходов в этот месяце");
 let expensesHowMatch2 = prompt("Во сколько обойдется?");
 
 let appData = {
     money: money,
     timeData: time,
-    expenses: {
-        "expensesMandatory" : "expensesHowMatch"
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
-}
+};
 
-console.log(appData["money"]);
-console.log(appData["timeData"]);
-console.log(appData["expenses"]);
+appData.expenses[expensesMustBe] = expensesHowMatch;
+appData.expenses[expensesMustBe2] = expensesHowMatch2;
 
+console.log(appData);
 
-alert( +money / days );
+alert( money / days );
