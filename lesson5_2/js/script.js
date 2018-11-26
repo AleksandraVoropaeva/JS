9138001d888fc4ac9794433284b8d785c871262a
Aleksandra <a.voropaeva88@gmail.com>
2018-11-26 05:38:09 +0300

    Первый коммит

---
+++ lesson5_2/js/script.js
@@ -0,0 +1,49 @@
+let calcStart = document.getElementById('start'),
+
+    budgetValue = document.getElementsByClassName('budget-value'),
+    dayBudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value'),
+
+    expensesItems = document.getElementsByClassName('expenses-item'),
+    optionalExpensesItems = document.querySelectorAll('optionalexpenses-item'),
+
+    expensesBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2],
+
+    income = document.querySelector('#income'),
+    checksavings = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+
+console.log(budgetValue);
+console.log(dayBudgetValue);
+console.log(levelValue);
+console.log(expensesValue);
+console.log(optionalExpensesValue);
+console.log(incomeValue);
+console.log(monthSavingsValue);
+console.log(yearSavingsValue);
+console.log(expensesItems);
+console.log(expensesBtn);
+console.log(optionalExpensesBtn);
+console.log(countBtn);
+console.log(income);
+
+console.log(checksavings);
+console.log(sum);
+console.log(percent);
+console.log(yearValue);
+console.log(monthValue);
+console.log(dayValue);
+
